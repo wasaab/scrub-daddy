9846b5165ad6252a5817b65bd5edd785d7f6bdab
Kent Hannigan <kent_hannigan@yahoo.com>
2017-10-24 22:29:05 -0400

    changed channel id

--- const.js
+++ const.js
@@ -17,7 +17,7 @@ define('BOT', new Discord.Client({
 define('LOG', require('winston'));
 
 define("LOOP_DELAY", 1500);							        //delay between each loop
-define('BOT_SPAM_CHANNEL_ID', '370626436609867786');		//listen's to messages from this channel
+define('BOT_SPAM_CHANNEL_ID', '372570540482887701');		//listen's to messages from this channel
 define('SCRUBS_CHANNEL_ID', '370626384059695107');		    //channel ID of scrubs text channel
 define('SERVER_ID', auth.serverID);				    		//Bed Bath Server ID
 define('DAYS', ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]);
