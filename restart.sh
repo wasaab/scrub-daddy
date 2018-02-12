26a89dce763f0d3a387211babf205f9b0d490774
Kent Hannigan <kent@vt.edu>
2018-02-12 00:28:43 -0500

    refactored restart script

--- restart.sh
+++ restart.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+
+update_and_sleep()
+{
+    git pull
+    sleep 5s
+}
+
 forever stopall 
 sleep 5s
-if [ "$1" != "" ]; then
-    if [ "$1" = "hard" ]; then
-        echo performing HARD git pull
-        git reset --hard HEAD
-        git pull
-    elif [ "$1" = "up" ]; then
-        echo performing standard git pull
-        git pull
-    fi
-    sleep 5s
+if [ "$1" = "hard" ]; then
+    echo performing HARD git pull
+    git reset --hard HEAD
+    update_and_sleep
+elif [ "$1" = "up" ]; then
+    echo performing standard git pull
+    update_and_sleep
 fi
 forever start src/bot.js
